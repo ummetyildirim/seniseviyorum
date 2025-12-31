f8e73decb1a47288d08d5f5720736c014d34d3e8
ummetyildirim <ummetyildirim2@gmail.com>
2025-12-31 22:20:15 +0300

    İlk commit

---
+++ config.js
@@ -0,0 +1,28 @@
+// 霸都丶傲天 2019年12月24日
+
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+    "Sevgilim",
+    "Şu an aramızda mesafeler var",
+    "Ama kalplerimiz hep yan yana",
+    "Her gün seni biraz daha özlüyorum",
+    "Geceler seni düşünerek geçiyor",
+    "Saatler kavuşmayı sayıyor",
+    "Az kaldı",
+    "Bir kaç ay sonra",
+    "Aynı şehirde uyanacağız",
+    "Sana sarılacağım",
+    "Elini hiç bırakmayacağım",
+    "Bu mesafe bitecek",
+    "Biz hep birlikteyiz",
+]
+
+,
+   wish: "Mesafe biter, biz kalırız ❤️",
+  //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    // 时间的格式很重要哦，一定要是下面这个格式!!
+    // 年年年年-月月-日日 时时:分分:秒秒
+    time: "2017-07-20 0:0:0",    //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
