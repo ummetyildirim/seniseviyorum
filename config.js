4ffb1f478569f71d77e6d6dc953239879d5dee79
ummetyildirim <115953550+ummetyildirim@users.noreply.github.com>
2025-12-31 23:56:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,22 +5,13 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
     "Sevgilim",
-    "Şu an aramızda mesafeler var",
+    "kalbim her gün sana daha da yaklaşıyor",
     "Ama kalplerimiz hep yan yana",
-    "Her gün seni biraz daha özlüyorum",
-    "Geceler seni düşünerek geçiyor",
-    "Saatler kavuşmayı sayıyor",
-    "Az kaldı",
-    "Bir kaç ay sonra",
-    "Aynı şehirde uyanacağız",
-    "Sana sarılacağım",
-    "Elini hiç bırakmayacağım",
-    "Bu mesafe bitecek",
-    "Biz hep birlikteyiz",
+   "Seni seviyorum"
 ]
 
 ,
-   wish: "Mesafe biter, biz kalırız ❤️",
+   wish: "❤️",
   //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
     // 时间的格式很重要哦，一定要是下面这个格式!!
     // 年年年年-月月-日日 时时:分分:秒秒
